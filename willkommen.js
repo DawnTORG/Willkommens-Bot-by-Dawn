905506c434bc1be774450b5ee9d3925f43958ba1
DawnTORG <41655849+DawnTORG@users.noreply.github.com>
2018-09-03 16:01:23 +0200

    Update willkommen.js

--- willkommen.js
+++ willkommen.js
@@ -14,7 +14,7 @@ member.send('```fix\nHerzlich Willkommen auf dem Discord Server von SimReportsRP
 '```css\nTeamSpeak 3 IP: [ simreportsrp.zap-ts3.com ]\n\nFiveM-Server IP: [ 185.249.197.141:30120 ] | link:```https://servers.fivem.net/#/servers/detail/185.249.197.141:30120\n```css\n\n\nInternetseite von SimReportsRP: [ simreportsrp.com ] | link:```http://simreportsrp.com/index.html\n```css\nInternetseite von unserem Partner Top Mods: [ www.topmods.de/wpnew/ ] | link:```https://topmods.de/wpnew/\n```css\nUm den Server zu unterstützen kannst du gerne spenden | link zum Spenden: ```https://paypal.me/pools/c/82u1RI28aZ\n\n\n```css\nBei Fragen oder Problemen bitte im TS in den Support-Raum kommen oder hier auf dem Discord in den Support-Channel schreiben\n```\n\n' +
 '```fix\nViel Spaß auf dem Server wünscht dir dein SimReportsRP-Team\n```\n');
 
-member.addRole('486083148254543892')
+
 });
 
 client.on('message', message => {
