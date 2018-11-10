aab3a0857643fd65f289f1c7fbce0cc2c819d24a
DawnTORG <41655849+DawnTORG@users.noreply.github.com>
2018-11-10 04:11:44 +0100

    Version 1.4 (Testversion)
    +Music function

--- willkommen.js
+++ willkommen.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const ytdl = require('ytdl-core');
 
 const prefix = "!";
 
@@ -38,6 +39,37 @@ client.on('message', message => {            //Liest nachrichten
 		}, 80000 * 1000);
 	}	
 });
+
+client.on('message', async msg => {
+	
+          if(msg.author.bot) return;
+		  
+		  const args = msg.content.split(' ');
+		  
+		  if(msg.content.startsWith(prefix + 'play')){
+			  const voiceChannel = msg.member.voiceChannel;
+			  
+			  if(!voiceChannel) return msg.channel.send('Dafür musst du in einem Sprach-Chat sein!');
+			  
+			  const permissions = voiceChannel.permissionsFor(msg.client.user);
+			  if(!permissions.has('CONNECT')){
+				  return msg.channel.send('Ich kann nicht beitrete, weil ich keine Berechtigung dazu habe!');
+			  }
+			  if(!permissions.has('SPEAK')) {
+				  return msg.channel.send('Ich kann nicht reden, weil ich keine Berechtigung dazu habe!');
+			  }
+			  
+			  var connection = await voiceChannel.join();
+			  
+			  const dispatcher = connection.playStream(ytdl(args[1]));
+		  dispatcher.setVolumeLogarithmic(5 / 5)
+		  }
+		  if(msg.content.toLowerCase() === prefix + 'stop'){
+			  			  const voiceChannel = msg.member.voiceChannel;
+						  voiceChannel.leave();
+		  }
+						 
+	});
 
 client.on('guildMemberAdd', member => {                 //wenn user beitritt
 	
