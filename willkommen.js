fd42ecb3c275aa7f387fd52c6b252e4e62c656fc
DawnTORG <41655849+DawnTORG@users.noreply.github.com>
2019-01-03 19:10:17 +0100

    Version 1.9
    All Informations about the current Version can be found in the ReadMe file

--- willkommen.js
+++ willkommen.js
@@ -27,7 +27,7 @@ client.on('message', msg => {            //Liest nachrichten
 	if(msg.author.id === banned){           //überprüfen ob user banned / J.S.
 		var embed = new Discord.RichEmbed(embed)
 		  .addField('Du wurdest von der anonymen Nachrichtenfunktion ausgeschlossen', 'Du wurdest vermutlich gebannt, da du die Regeln missachtest hast')
-		  .addField('Du denkst das ist zu unrecht?', 'Dann schreibe eine Nachricht in den Support-Channel und achte darauf, dass du die Uhrzeit deiner letzten Admin-Anfrage dazu schreibst.')
+		  .addField('Du denkst das ist zu unrecht?', 'Dann schreibe eine Nachricht in den Support-Channel und achte darauf, dass du die Uhrzeit deiner letzten Admin-Anfrage und deine Anfrage id (' + msg.author.id + ') dazu schreibst.')
 		  .setColor('RED')
 		msg.author.sendEmbed(embed)
 		
@@ -61,7 +61,7 @@ client.on('message', msg => {            //Liest nachrichten
 		.addField(':arrow_right: TeamSpeak 3 IP:', 'SRB.zap-ts3.com')
 		.addField(':arrow_right: FiveM-Server IP:', '134.255.220.185:32024\nlink: https://servers.fivem.net/#/servers/detail/134.255.220.185:32024')
 		.addField(':arrow_right: Internetseite von SimReportsRP:', 'simreportsrp.com\nlink: http://simreportsrp.com/index.html')
-                .addField(':arrow_right: Forum von SimReportsRP:', 'www.simreportsforum.de/\nlink: http://www.simreportsforum.de/')
+        .addField(':arrow_right: Forum von SimReportsRP:', 'www.simreportsforum.de/\nlink: http://www.simreportsforum.de/')
 		.addField(':arrow_right: Internetseite von unserem Partner Top Mods:', 'www.topmods.de/wpnew/\nlink: https://topmods.de/wpnew/')
 		.addField(':arrow_right: Um den Server zu unterstützen kannst du gerne hier spenden:', 'https://paypal.me/pools/c/82u1RI28aZ')
 		.addField(':exclamation: Bei Fragen oder Problemen folgendes machen: :exclamation:', 'Im TS3 in den Support-Raum kommen oder hier auf dem Discord-Server in den Support-Channel schreiben')
@@ -72,6 +72,8 @@ client.on('message', msg => {            //Liest nachrichten
 		
 	msg.author.sendEmbed(embed);                      //Nachricht mit allen infos (Privat)
 	}
+	
+	
 	
 });
 
