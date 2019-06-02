48e5ab94528b4afcc496ab9ed52c7236e8f4ca03
DawnTORG <41655849+DawnTORG@users.noreply.github.com>
2019-06-02 02:07:15 +0200

    Update willkommen.js

--- willkommen.js
+++ willkommen.js
@@ -7,29 +7,58 @@ client.on('ready', () => {
 	console.log("Bot jetzt angeschaltet\n\n")               //Konsolen Log
 });
 
-client.on('message', msg => {            //Liest nachrichten
-	
-       if(msg.content.toLowerCase() === prefix + 'status'){          //check 
-		msg.channel.send('```\n Checked GitHub status. Status: WORKING \n Checked Code for errors. Status: WORKING \n Checked Heroku status. Status: WORKING```')
+client.on('message', msg => {
+
+if(msg.author.bot)return;
+
+       if(msg.content.toLowerCase() === prefix + 'status'){          //check status of bot
+
+       	 msg.delete();
+
+         let statuschannel = client.channels.get('492729308494954496')
+
+       	if(msg.channel != statuschannel){
+
+            var embed = new Discord.RichEmbed()
+                .addField('Du kannst diesen befehl nicht ausführen!', '(' + msg.content + ')')
+                .addField('Der Grund, weshalb du das nicht kannst: ', 'Dir fehlen die nötigen Berechtigungen oder du hast ihn in dem Falschen Channel ausgeführt')
+                .setColor('RED')
+             msg.author.sendEmbed(embed)
+
+       		return;
+       	}
+
+		msg.channel.send('```\n Checked GitHub status. Status: WORKING \n Checked Code for faults. Status: WORKING \n Checked Heroku status. Status: WORKING```')
 	}
 	
-	/*if(msg.content.startsWith(prefix + 'admin')){              //Anonyme Admin nachrichten funktion / 03-07-03
+	if(msg.content.startsWith(prefix + 'admin')){              //Anonyme Admin nachrichten funktion / 03-07-03
 		
 		msg.delete();
 
-	const banned = '';             //gebannte leute / M-HÜL-GK
+	const banned = '215488160304463872';             //gebannte leute / M-HÜL-GK
 	let achannel = client.channels.get('517449527947427851')                 //define achannel = channel für nachricht an admins
+	let achannel2 = client.channels.get('517452924830613505')                //define achannel2 = channel für nachricht von usern
 	
 	nachricht = msg.content.slice (6);
-	
-	if(msg.author.bot)return;
+
 	if(msg.author.id === banned){           //überprüfen ob user banned / J.S.
 		var embed = new Discord.RichEmbed(embed)
-		  .addField('Du wurdest von der anonymen Nachrichtenfunktion ausgeschlossen', 'Du wurdest vermutlich gebannt, da du die Regeln missachtest hast')
-		  .addField('Du denkst das ist zu unrecht?', 'Dann schreibe eine Nachricht in den Support-Channel und achte darauf, dass du die Uhrzeit deiner letzten Admin-Anfrage und deine Anfrage id (' + msg.author.id + ') dazu schreibst.')
+		  .addField('Du wurdest von der anonymen Nachrichtenfunktion ausgeschlossen', 'Du wurdest vermutlich gebannt, da du gegen die Regeln verstoßen hast')
+		  .addField('Du denkst das ist zu unrecht?', 'Dann schreibe eine Nachricht in den Support-Channel und achte darauf, das du die Uhrzeit deiner letzten Admin-Anfrage und deine Anfrage id (' + msg.author.id + ') dazu schreibst.')
 		  .setColor('RED')
 		msg.author.sendEmbed(embed)
 		
+		return;
+	}
+
+	if(msg.channel != achannel2){
+
+		var embed = new Discord.RichEmbed()
+		   .addField('Du kannst diesen Befehl nicht ausführen!', '(' + msg.content + ')')
+		   .addField('Der Grund, weshalb du das nicht kannst: ', 'Du hast den Befehl in dem falschen Channel ausgeführt')
+		   .setColor('RED')
+		msg.author.sendEmbed(embed)
+
 		return;
 	}
 	
@@ -38,15 +67,15 @@ client.on('message', msg => {            //Liest nachrichten
 	var embed = new Discord.RichEmbed()
 	     .addField('Ein User möchte anonym etwas melden. Hier ist das Problem:', '=> ' + nachricht)
 		 .addField('Die ID der Anfrage ist:', '' + msg.author.id)
-		 .setColor('RED')
+		 .setColor('GREEN')
 	achannel.sendEmbed(embed)
 	
 	var embed = new Discord.RichEmbed()
 	    .addField(':rotating_light: Bestätigung deiner Adminanfrage :rotating_light:', 'Deine Anfrage wurde anonym gesendet und ein Admin kümmert sich so schnell wie möglich darum!')
-		.setColor('RED')
+		.setColor('GREEN')
 	msg.author.sendEmbed(embed)
 	
-	}*/
+	}
 
 	
 		if(msg.content.toLowerCase() === prefix + 'info')
@@ -60,16 +89,16 @@ client.on('message', msg => {            //Liest nachrichten
 		.addField(':arrow_right: TeamSpeak 3 IP:', 'SRB.zap-ts3.com')
 		.addField(':arrow_right: FiveM-Server IP:', '109.230.235.145:30120\nlink: https://servers.fivem.net/servers/detail/109.230.235.145:30120')
 		.addField(':arrow_right: Internetseite von SimReportsRP:', 'www.simreportsrp.de/\nlink: http://www.simreportsrp.de/')
-                .addField(':arrow_right: Forum von SimReportsRP:', 'www.simreportsforum.de/\nlink: http://www.simreportsforum.de/')
+        .addField(':arrow_right: Forum von SimReportsRP:', 'www.simreportsforum.de/\nlink: http://www.simreportsforum.de/')
 		.addField(':arrow_right: YouTube-Seite von SimReportsRP:', 'www.youtube.com/channel/UCt7RQcNADxo0hw_xZzMTVNQ\nlink: https://www.youtube.com/channel/UCt7RQcNADxo0hw_xZzMTVNQ')
-                .addField(':arrow_right: Twitch-Seite von SimReportsRP:', 'www.twitch.tv/simreportsrp\nlink: https://www.twitch.tv/simreportsrp')            
+        .addField(':arrow_right: Twitch-Seite von SimReportsRP:', 'www.twitch.tv/simreportsrp\nlink: https://www.twitch.tv/simreportsrp')            
 		.addField(':arrow_right: Internetseite von unserem Partner Top Mods:', 'www.topmods.de/wpnew/\nlink: https://topmods.de/wpnew/')
 		.addField(':arrow_right: Um den Server zu unterstützen kannst du gerne hier spenden:', 'https://paypal.me/pools/c/82u1RI28aZ')
 		.addField(':exclamation: Bei Fragen oder Problemen folgendes machen: :exclamation:', 'Im TS3 in den Support-Raum kommen oder hier auf dem Discord-Server in den Support-Channel schreiben')
 		    
 		.addField(':star: Viel Spaß auf SimReportsRP :star:', 'Wünscht dir dein SimReportsRP Team')
 		
-		.setColor('GREEN')
+		.setColor('ORANGE')
 		
 	msg.author.sendEmbed(embed);                      //Nachricht mit allen infos (Privat)
 	}
@@ -81,14 +110,12 @@ else if(msg.content.startsWith(prefix + 'announce')){        //Announcment comma
 			let bchannel = client.channels.get('492729308494954496')     //Festlegung von Befehls Channel
 		
 			msg.delete();
-			if(msg.author.bot){return;}          //falls bot auto = bot => Return
-		     else if(msg.channel != bchannel){      //Falls channel ist nicht Befehlschannel
+
+		    if(msg.channel != bchannel){      //Falls channel ist nicht Befehlschannel
 			
 			var embed = new Discord.RichEmbed()
-			
-			.addField('Dein Versuch den Befehl !announce zu benutzen ist aus folgendem Grund fehlgeschlagen', 'Dir fehlen die nötigen Berechtigungen oder du hast den Befehl in dem Falschen Channel benutzt')
-			.setColor('RED')
-			
+                .addField('Du kannst diesen befehl nicht ausführen!', '(' + msg.content + ')')
+                .addField('Der Grund, weshalb du das nicht kannst: ', 'Die fehlen die nötigen Berechtigungen oder du hast ihn in dem Falschen Channel ausgeführt')
 			msg.author.sendEmbed(embed);
 			
 			return;
@@ -100,12 +127,13 @@ else if(msg.content.startsWith(prefix + 'announce')){        //Announcment comma
 	
 	return;
 	}
-	
+
 });
 
+
 client.on('guildMemberAdd', member => {                 //wenn user beitritt
-	
-	let wchannel = client.channels.get('563089119647891483')
+
+		let wchannel = client.channels.get('563089119647891483')
 		
     var embed = new Discord.RichEmbed()
 	    
@@ -113,16 +141,16 @@ client.on('guildMemberAdd', member => {                 //wenn user beitritt
 		.addField(':arrow_right: TeamSpeak 3 IP:', 'SRB.zap-ts3.com')
 		.addField(':arrow_right: FiveM-Server IP:', '109.230.235.145:30120\nlink: https://servers.fivem.net/servers/detail/109.230.235.145:30120')
 		.addField(':arrow_right: Internetseite von SimReportsRP:', 'simreportsrp.de/\nlink: http://www.simreportsrp.de/')
-                .addField(':arrow_right: Forum von SimReportsRP:', 'www.simreportsforum.de/\nlink: http://www.simreportsforum.de/')
-                .addField(':arrow_right: YouTube-Seite von SimReportsRP:', 'www.youtube.com/channel/UCt7RQcNADxo0hw_xZzMTVNQ\nlink: https://www.youtube.com/channel/UCt7RQcNADxo0hw_xZzMTVNQ')
-                .addField(':arrow_right: Twitch-Seite von SimReportsRP:', 'www.twitch.tv/simreportsrp\nlink: https://www.twitch.tv/simreportsrp')
+        .addField(':arrow_right: Forum von SimReportsRP:', 'www.simreportsforum.de/\nlink: http://www.simreportsforum.de/')
+        .addField(':arrow_right: YouTube-Seite von SimReportsRP:', 'www.youtube.com/channel/UCt7RQcNADxo0hw_xZzMTVNQ\nlink: https://www.youtube.com/channel/UCt7RQcNADxo0hw_xZzMTVNQ')
+        .addField(':arrow_right: Twitch-Seite von SimReportsRP:', 'www.twitch.tv/simreportsrp\nlink: https://www.twitch.tv/simreportsrp')
 		.addField(':arrow_right: Internetseite von unserem Partner Top Mods:', 'www.topmods.de/wpnew/\nlink: https://topmods.de/wpnew/')
 		.addField(':arrow_right: Um den Server zu unterstützen kannst du gerne hier spenden:', 'https://paypal.me/pools/c/82u1RI28aZ')
 		.addField(':exclamation: Bei Fragen oder Problemen folgendes machen: :exclamation:', 'Im TS3 in den Support-Raum kommen oder hier auf dem Discord-Server in den Support-Channel schreiben')
 
 		.addField(':star: Viel Spaß auf SimReportsRP :star:', 'Wünscht dir dein SimReportsRP Team')
 		
-		.setColor('GREEN')
+		.setColor('ORANGE')
 		
 	member.sendEmbed(embed);                      //Nachricht mit allen infos (Privat)
 	
